481b1b8a2ec04453c4726468855cec46d38c24b2
Stephen Li <stephenli.ca@gmail.com>
2014-10-25 04:51:24 -0400

    Added yaml generated from GitHub api

--- README.md
+++ README.md
@@ -7,3 +7,10 @@ Building Less
 ```
 lessc -x assets/less/main.less assets/css/main.css
 ```
+
+Generate Classmates
+---
+
+```
+python _scripts/generate_classmates.py > _data/classmates.yaml
+```
---
+++ _scripts/generate_classmates.py
@@ -0,0 +1,24 @@
+import requests
+import yaml
+
+outputArray = []
+
+def getJSON(url):
+    r = requests.get(url)
+    return r.json()
+
+members = getJSON('https://api.github.com/orgs/inseption/members')
+for member in members:
+    memberDetails = getJSON(member['url'])
+    outputArray.append({
+        'avatar_url'  : (memberDetails['avatar_url'] or '').strip(),
+        'github_name' : (memberDetails['login'] or '').strip(),
+        'github_url'  : (memberDetails['html_url'] or '').strip(),
+        'name'        : (memberDetails['name'] or '').strip(),
+        'company'     : (memberDetails['company'] or '').strip(),
+        'website'     : (memberDetails['blog'] or '').strip(),
+        'location'    : (memberDetails['location'] or '').strip(),
+    });
+
+outputArray.sort(key=lambda x: x['name'])
+print yaml.safe_dump(outputArray, encoding='utf-8', allow_unicode=True)
